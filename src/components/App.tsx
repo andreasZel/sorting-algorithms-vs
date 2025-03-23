a314fb9d8621e314391269ad6352e5a85da3a259
Andreas Zelios <a.zelios@satways.net>
2025-03-24 00:01:24 +0200

    used finish in log to avoid build error

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -18,6 +18,8 @@ const App: React.FC = () => {
   const [disableButtons, enableDisableBtns] = useState(false);
   const [finished, alterFinished] = useState(0);
 
+  console.log(finished)
+
   if (data.length > 0) {
     return (
       <div className='display_conteiner' onClick={() => {
@@ -27,16 +29,16 @@ const App: React.FC = () => {
       }
       }>
         <div className="display_grid">
-          <AlgorithmLegend algorithm_name={"Insertion Sort"} algorithm={1} data={data} shouldRunAll={shouldRunAll} overalComputing={overalComputing} alterOveralComputing={alterOveralComputing} alterFinished={alterFinished} enableDisableBtns={enableDisableBtns} alterShouldRunAll={alterShouldRunAll}/>
-          <AlgorithmLegend algorithm_name={"Selection Sort"} algorithm={2} data={data} shouldRunAll={shouldRunAll} overalComputing={overalComputing} alterOveralComputing={alterOveralComputing} alterFinished={alterFinished} enableDisableBtns={enableDisableBtns} alterShouldRunAll={alterShouldRunAll}/>
-          <AlgorithmLegend algorithm_name={"Bubble Sort"} algorithm={3} data={data} shouldRunAll={shouldRunAll} overalComputing={overalComputing} alterOveralComputing={alterOveralComputing} alterFinished={alterFinished} enableDisableBtns={enableDisableBtns} alterShouldRunAll={alterShouldRunAll}/>
-          <AlgorithmLegend algorithm_name={"Merge Sort"} algorithm={4} data={data} shouldRunAll={shouldRunAll} overalComputing={overalComputing} alterOveralComputing={alterOveralComputing} alterFinished={alterFinished} enableDisableBtns={enableDisableBtns} alterShouldRunAll={alterShouldRunAll}/>
+          <AlgorithmLegend algorithm_name={"Insertion Sort"} algorithm={1} data={data} shouldRunAll={shouldRunAll} overalComputing={overalComputing} alterOveralComputing={alterOveralComputing} alterFinished={alterFinished} enableDisableBtns={enableDisableBtns} alterShouldRunAll={alterShouldRunAll} />
+          <AlgorithmLegend algorithm_name={"Selection Sort"} algorithm={2} data={data} shouldRunAll={shouldRunAll} overalComputing={overalComputing} alterOveralComputing={alterOveralComputing} alterFinished={alterFinished} enableDisableBtns={enableDisableBtns} alterShouldRunAll={alterShouldRunAll} />
+          <AlgorithmLegend algorithm_name={"Bubble Sort"} algorithm={3} data={data} shouldRunAll={shouldRunAll} overalComputing={overalComputing} alterOveralComputing={alterOveralComputing} alterFinished={alterFinished} enableDisableBtns={enableDisableBtns} alterShouldRunAll={alterShouldRunAll} />
+          <AlgorithmLegend algorithm_name={"Merge Sort"} algorithm={4} data={data} shouldRunAll={shouldRunAll} overalComputing={overalComputing} alterOveralComputing={alterOveralComputing} alterFinished={alterFinished} enableDisableBtns={enableDisableBtns} alterShouldRunAll={alterShouldRunAll} />
           <AlgorithmLegend algorithm_name={"Tree Sort"} algorithm={5} data={data} shouldRunAll={shouldRunAll} overalComputing={overalComputing} alterOveralComputing={alterOveralComputing} alterFinished={alterFinished} enableDisableBtns={enableDisableBtns} alterShouldRunAll={alterShouldRunAll} />
         </div>
         {!isMenuVisible && <button className='menu' title='menu' onClick={() => [alterMenuState((true))]}>☰</button>}
 
         {isMenuVisible && <div className='menu_btns'>
-          <button title='run all' disabled={overalComputing || disableButtons} onClick={async() => { changedata([...data]); await delay(300); alterShouldRunAll(true); }} className='runAllAlgorithmsBtn'>Run All</button>
+          <button title='run all' disabled={overalComputing || disableButtons} onClick={async () => { changedata([...data]); await delay(300); alterShouldRunAll(true); }} className='runAllAlgorithmsBtn'>Run All</button>
           <button title='reset' disabled={overalComputing || disableButtons} onClick={() => { enableDisableBtns(false); changedata([...data]); }} onMouseOut={() => { alterRotation("horizontal-tb") }} onMouseOver={() => { alterRotation("vertical-rl") }}><span style={{ writingMode: rotate }}>↻</span></button>
           <button title='go back' disabled={overalComputing || disableButtons} onClick={() => { alterShouldRunAll(false); enableDisableBtns(false); changedata([]) }}>↩</button>
         </div>}
