2b2d735f163632fc9438919748279407fad38f9d
Andreas Zelios <a.zelios@satways.net>
2025-04-09 00:45:23 +0300

    small tweaks

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -2,41 +2,41 @@ import "../css/PopUp.css"
 import { useState } from 'react';
 
 interface Props {
-    changedata:React.Dispatch<React.SetStateAction<number[]>>;
-  }
+    changedata: React.Dispatch<React.SetStateAction<number[]>>;
+}
 
-const PopUp = ({changedata}:Props) => {
-    
+const PopUp = ({ changedata }: Props) => {
+
     let [numberOfElements, changeInput] = useState<number>(0);
 
-    const handleInput = (input:string) => {
+    const handleInput = (input: string) => {
 
-        let tempnum:number = parseInt(input);
+        let tempnum: number = parseInt(input);
 
         if (isNaN(tempnum)) {
             changeInput(0);
         } else {
-            changeInput(tempnum); 
+            changeInput(tempnum);
         }
     }
 
     const applyRandomValues = () => {
-        let i:number = 0;
-        let temparray:number[] = [];
+        let i: number = 0;
+        let temparray: number[] = [];
 
-        for (i; i < numberOfElements; i++){
+        for (i; i < numberOfElements; i++) {
             temparray.push(Math.floor((Math.random() * 100) + 1));
         }
         changedata(temparray);
     }
 
-    return(
+    return (
         <div id="pop_up_overlay">
             <div id="pop_up_conteiner">
-                <h3>How many numbers to sort?</h3>
+                <h3>How many numbers to sort ?</h3>
                 <div className="break"></div>
                 <div id="input_conteiner">
-                    <input onChange={(e) => {handleInput(e.target.value)}} value={numberOfElements}/>
+                    <input onChange={(e) => { handleInput(e.target.value) }} value={numberOfElements} />
                     <button onClick={applyRandomValues}>Go</button>
                 </div>
             </div>
